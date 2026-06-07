752da9dec724bfb4735c269bfa81d9be3a9e7478
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2026-06-07 19:53:49 +0700

    feat: add future server utilization estimation to dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from app.cli import init_db, seed, db_reset
 
 db = SQLAlchemy()
-login_manager = LoginManager()
+# login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
@@ -13,16 +13,16 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = "auth.login"
-    login_manager.login_message = "Silakan login terlebih dahulu."
-    login_manager.login_message_category = "warning"
+    # login_manager.init_app(app)
+    # login_manager.login_view = "auth.login"
+    # login_manager.login_message = "Silakan login terlebih dahulu."
+    # login_manager.login_message_category = "warning"
 
-    from app.controllers.auth_controller import auth_bp
+    # from app.controllers.auth_controller import auth_bp
     from app.controllers.main_controller import main_bp
     from app.controllers.dashboard_controller import dashboard_bp
 
-    app.register_blueprint(auth_bp)
+    # app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(dashboard_bp)
     
