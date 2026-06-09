049100fa2cf0a5714cd304d8ffc8b5fd0eb949e5
iki <rifqiirfansyah518@gmail.com>
2026-06-09 23:32:53 +0700

    feat: add forgot password with otp SMTP gogle gmail & add requirements Flask-Mail

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from app.cli import init_db, seed, db_reset
+from flask_mail import Mail
 
+mail = Mail()
 db = SQLAlchemy()
 login_manager = LoginManager()
 
@@ -13,6 +15,7 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    mail.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     login_manager.login_message = "Silakan login terlebih dahulu."
--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -2,8 +2,12 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app.services import AuthService
 from app.repositories import DatasetRepository
-from app import login_manager
+from app import db, mail, login_manager
 from app.models import User
+from werkzeug.security import generate_password_hash
+from flask_mail import Mail, Message
+from flask import session
+import secrets, random
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -39,6 +43,89 @@ def logout():
     flash("Anda telah logout.", "info")
     return redirect(url_for("auth.login"))
 
+@auth_bp.route("/forgot-password", methods=["GET", "POST"])
+def forgot_password():
+
+    if request.method == "POST":
+        email = request.form.get("email", "").strip()
+        user = AuthService.get_user_by_email(email)
+        otp = str(random.randint(100000, 999999))
+        
+        session["reset_email"] = email
+        session["reset_otp"] = otp
+        
+        if not user:
+            flash("Email tidak ditemukan.", "danger")
+            return redirect(url_for("auth.forgot_password"))
+
+        new_password = secrets.token_urlsafe(8)
+
+        user.password = generate_password_hash(new_password)
+        db.session.commit()
+
+        msg = Message(
+            subject="Kode Reset Password",
+            recipients=[email]
+        )
+
+        msg.body = f"""
+        Kode verifikasi reset password Anda:
+
+        {otp}
+
+        Kode berlaku selama 10 menit.
+        """
+
+        mail.send(msg)
+
+        flash("Kode Verifikasi telah dikirim ke email Anda.", "success")
+        return redirect(url_for("auth.verify_otp"))
+
+    return render_template("auth/forgot-password.html")
+
+@auth_bp.route("/verify-otp", methods=["GET", "POST"])
+def verify_otp():
+
+    if request.method == "POST":
+
+        otp = request.form.get("otp")
+
+        if otp != session.get("reset_otp"):
+            flash("Kode OTP salah.", "danger")
+            return redirect(url_for("auth.verify_otp"))
+
+        return redirect(url_for("auth.reset_password"))
+
+    return render_template("auth/verify-otp.html")
+
+@auth_bp.route("/reset-password", methods=["GET", "POST"])
+def reset_password():
+
+    if request.method == "POST":
+
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        if password != confirm_password:
+            flash("Konfirmasi password tidak cocok.", "danger")
+            return redirect(url_for("auth.reset_password"))
+
+        email = session.get("reset_email")
+
+        user = AuthService.get_user_by_email(email)
+
+        user.password = generate_password_hash(password)
+
+        db.session.commit()
+
+        session.pop("reset_email", None)
+        session.pop("reset_otp", None)
+
+        flash("Password berhasil diubah.", "success")
+
+        return redirect(url_for("auth.login"))
+
+    return render_template("auth/reset-password.html")
 
 @auth_bp.route("/profile", methods=["GET"])
 @login_required
