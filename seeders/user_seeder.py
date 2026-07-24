ebe4e60647e1c8aad6953d42af20b54a53060740
Muhammad Fachry Khairiansyah <khairiansyahfachry1@gmail.com>
2026-07-24 13:19:23 +0700

    fix: remove user entries from seeder

--- seeders/user_seeder.py
+++ seeders/user_seeder.py
@@ -6,8 +6,6 @@ from app.models import User
 def seed_users() -> None:
     default_users = [
         {"email": "admin@forecasting.com", "password": "admin123"},
-        {"email": "kensio683@gmail.com", "password": "admin123"},
-        {"email": "khairiansyahfachry1@gmail.com", "password": "admin123"},
     ]
 
     for data in default_users:
